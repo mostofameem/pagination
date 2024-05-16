c813df1127312549321c6a2cc1da5359c31937f3
mostofameem <168959686+mostofameem@users.noreply.github.com>
2024-05-16 18:30:19 +0600

    Delete products directory

--- products/models/info.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Product struct {
-	ID       string `json:"id" db:"id"`
-	Name     string `json:"name" db:"name"`
-	Category string `json:"category" db:"category"`
-	Price    string `json:"price" db:"price"`
-	Quantity string `json:"quantity" db:"quantity"`
-}
